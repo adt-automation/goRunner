ec775c61e97c486e616500b4ad877b49bd64e634
dougwatson <douglaswatson@gmail.com>
2017-04-24 22:54:06 -0500

    first go files

---
+++ goRunner.go
@@ -0,0 +1,252 @@
+package main
+
+//author: Doug Watson
+
+//macOS will need more open files than the default 256 if you want to run over a couple hundred goroutines
+//launchctl limit maxfiles 10200
+
+//For a really big test, if you need a million open files on a mac:
+//nvram boot-args="serverperfmode=1"
+//shutdown -r now
+//launchctl limit maxfiles 999999
+//ulimit -n 999999
+
+import (
+	"bufio"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"regexp"
+	"runtime"
+	"runtime/pprof"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+
+	runner "./golib"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
+var (
+	clients        int
+	secsTimedTest  int
+	noRampUp       bool
+	debug          bool
+	dump           bool
+	connectTimeout int
+	writeTimeout   int
+	readTimeout    int
+	targetTPS      float64
+	baseUrl        string
+	configFile     string
+	inputFile      string
+	inputFile1     string
+	delimeter      string
+	headerExit     bool
+	noHeader       bool
+
+//	wg             sync.WaitGroup
+)
+
+type MyConn struct {
+	net.Conn
+	readTimeout  time.Duration
+	writeTimeout time.Duration
+	result       *runner.Result
+}
+
+func init() {
+	flag.IntVar(&clients, "c", 100, "Number of concurrent clients to launch.")
+	flag.IntVar(&secsTimedTest, "t", 0, "Time in seconds for a timed load test")
+	flag.StringVar(&inputFile, "f", "", "Read input from file rather than stdin")
+	flag.StringVar(&inputFile1, "fh", "", "Read input from csv file which has a header row")
+	flag.BoolVar(&noRampUp, "nr", false, "Do not ramp up to total concurrent number of clients")
+	flag.Float64Var(&targetTPS, "targetTPS", 1000000, "The default max TPS is set to 1 million. Good luck reaching this :p")
+	flag.StringVar(&baseUrl, "baseUrl", "", "The host to test. Example https://test2.someserver.org")
+	flag.BoolVar(&debug, "debug", false, "Show the body returned for the api call")
+	flag.BoolVar(&dump, "dump", false, "Show the HTTP dump for the api call")
+	flag.StringVar(&configFile, "configFile", "config.ini", "Config file location")
+	flag.StringVar(&delimeter, "d", ",", "Output file delimeter")
+	flag.BoolVar(&headerExit, "hx", false, "Print output header row and exit")
+	flag.BoolVar(&noHeader, "nh", false, "Suppress output header row. Ignored if hx is set")
+	flag.IntVar(&readTimeout, "readtimeout", 30, "Timeout in seconds for the target API to send the first response byte. Default 30 seconds")
+}
+
+/*
+func generateInput() chan string {
+	lines := make(chan string)
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			lines <- scanner.Text()
+		}
+		//scanner.Err()
+		fmt.Printf("scanner done:\n\n")
+	}()
+	return lines
+}
+*/
+func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+	runner.Delimeter = delimeter //set the delimeter we want for the output log in the imported runner package
+	if !headerExit && baseUrl == "" {
+		fmt.Fprintf(os.Stderr, "\nPlease provide the baseUrl...\n\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+	trafficChannel := make(chan string)
+	//	startTraffic(trafficChannel) //start reading on the channel
+
+	if *runner.Verbose {
+		println("verbose")
+	}
+
+	overallStartTime := time.Now()
+	baseUrlFilter := regexp.MustCompile(baseUrl)
+	configuration := runner.NewConfiguration2(configFile)
+	results := make(map[int]*runner.Result)
+	signalChannel := make(chan os.Signal, 2)
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		_ = <-signalChannel
+		runner.ExitWithStatus(results, overallStartTime)
+	}()
+	goMaxProcs := os.Getenv("GOMAXPROCS")
+	if goMaxProcs == "" {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+	}
+
+	if !headerExit {
+		printSessionSummary(configuration, configFile)
+	}
+
+	var rampUpDelayMs time.Duration = time.Duration(0)
+	if !headerExit && !noRampUp && clients > 1 {
+		rampUpDelayMs = calcRampUpDelay(configuration)
+		fmt.Fprintf(os.Stderr, "Spacing sessions %v apart to ramp up to %d client sessions\n", rampUpDelayMs, clients)
+	}
+	transportConfig := &tls.Config{InsecureSkipVerify: true} //allow wrong ssl certs
+	tr := &http.Transport{TLSClientConfig: transportConfig}
+	tr.ResponseHeaderTimeout = time.Second * time.Duration(readTimeout)
+	var done sync.WaitGroup
+	var stopTime time.Time // client loops will work to the end of trafficChannel unless we explicitly init a stopTime
+	if secsTimedTest > 0 {
+		stopTime = time.Now().Add(time.Duration(secsTimedTest) * time.Second)
+	}
+	for i := 0; i < clients; i++ {
+		done.Add(1)
+		result := &runner.Result{}
+		results[i] = result
+		clientDelay := rampUpDelayMs * time.Duration(i)
+		go client(tr, configuration, result, &done, trafficChannel, i, baseUrlFilter, stopTime, clientDelay)
+	}
+
+	if len(inputFile1) > 0 {
+		if len(inputFile) > 0 {
+			fmt.Fprintf(os.Stderr, "don't use -f and -fh together\n")
+			os.Exit(1)
+		} else {
+			inputFile = inputFile1
+		}
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	if len(inputFile) > 0 {
+		file, err := os.Open(inputFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err.Error())
+			os.Exit(1)
+		}
+		defer file.Close()
+		scanner = bufio.NewScanner(file)
+	}
+
+	_ = scanner.Scan()
+	if !noHeader {
+		runner.PrintLogHeader(scanner.Text(), len(inputFile1) > 0)
+		if headerExit {
+			os.Exit(0)
+		}
+	}
+	if len(inputFile1) > 0 && runner.HasInputColHeaders() == false {
+		// name our column headers
+		runner.HeadInputColumns(scanner.Text())
+	} else {
+		// anonymous column headers, put work into the channel instead
+		trafficChannel <- scanner.Text()
+	}
+
+	for scanner.Scan() {
+		trafficChannel <- scanner.Text() //put work into the channel from Stdin
+	}
+	close(trafficChannel)
+	done.Wait()
+	runner.ExitWithStatus(results, overallStartTime)
+}
+
+func calcRampUpDelay(cfg *runner.CfgStruct) time.Duration {
+	if clients == 0 {
+		return time.Duration(0)
+	} else {
+		dur := runner.EstimateSessionTime(cfg)
+		return dur / time.Duration(clients)
+	}
+}
+
+//one client per -c=thread
+//func noRedirect(req *http.Request, via []*http.Request) error {
+//	return errors.New("Don't redirect!")
+//}
+func client(tr *http.Transport, configuration *runner.CfgStruct, result *runner.Result, done *sync.WaitGroup, trafficChannel chan string, clientId int, baseUrlFilter *regexp.Regexp, stopTime time.Time, initialDelay time.Duration) {
+	defer done.Done()
+	// strangely, this sleep cannot be moved outside the client function
+	// or all client threads will wait until the last one is spawned before starting their own execution loops
+	time.Sleep(initialDelay)
+
+	msDelay := 0
+	grep1, grep2 := "", ""
+	for id := range trafficChannel {
+		//read in the line. either a generated id or (TODO) a url
+		//		id := <-trafficChannel //urlTemplate : 100|10000|about
+
+		//Do Heartbeat
+		// cookieMap and sessionVars should start fresh every time we start a DoReq session
+		var cookieMap = make(map[string]*http.Cookie)
+		var sessionVars = make(map[string]string)
+		runner.DoReq(0, id, configuration, result, clientId, baseUrl, baseUrlFilter, msDelay, tr, cookieMap, sessionVars, grep1, grep2, stopTime, 0.0) //val,resp, err
+		msDelay = runner.PostSessionDelay
+	}
+	if time.Now().Before(stopTime) {
+		fmt.Fprintf(os.Stderr, "client %d ran out of test input %.2fs before full test time\n", clientId, stopTime.Sub(time.Now()).Seconds())
+	}
+}
+
+func printSessionSummary(configuration *runner.CfgStruct, configFile string) {
+	fmt.Fprintf(os.Stderr, "GORUNNER\n")
+	if len(configuration.Version.ConfigVersion) > 5 {
+		fmt.Fprintf(os.Stderr, "Configuration:                  %s version %s\n", configFile, configuration.Version.ConfigVersion[5:len(configuration.Version.ConfigVersion)-1])
+	} else {
+		fmt.Fprintf(os.Stderr, "Configuration:                  %s\n", configFile)
+	}
+	fmt.Fprintf(os.Stderr, "Session profile:                %d requests: %s\n", len(runner.CommandQueue), strings.Join(runner.CommandQueue, ", "))
+	fmt.Fprintf(os.Stderr, "Estimated session time:         %v\n", runner.EstimateSessionTime(configuration))
+	fmt.Fprintf(os.Stderr, "Simultaneous sessions:          %d\n", clients)
+	fmt.Fprintf(os.Stderr, "API host:                       %s\n", baseUrl)
+	const layout = "2006-01-02 15:04:05"
+	fmt.Fprintf(os.Stderr, "Test start time:                %v\n", time.Now().Format(layout))
+}
---
+++ golib/macro/runner_macro.go
@@ -0,0 +1,332 @@
+package macro
+
+import (
+	"crypto/md5"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type UnixtimeMacro struct {
+	duration time.Duration
+	format   string
+}
+
+var Md5Inputs map[string]string = make(map[string]string)
+var Base64Inputs map[string]string = make(map[string]string)
+var UnixtimeMacros map[string]UnixtimeMacro
+var PrintTimeMacros map[string]UnixtimeMacro
+var CommandMacros = make(map[string][]string)
+var Md5Macros = make(map[string][]string)
+var Base64Macros = make(map[string][]string)
+var KvDelimeter = ","
+var inputColHeaders = make(map[string]int)
+var csvrx = regexp.MustCompile("{%CSV\\[(\\d+)\\]}")
+
+func HasInputColHeaders() bool {
+	return len(inputColHeaders) > 0
+}
+
+func HeadInputColumns(kvText string) {
+	for i, val := range strings.Split(kvText, KvDelimeter) {
+		inputColHeaders["{%"+val+"}"] = i // initialize these as macros
+	}
+}
+
+func inputColumnIndex(macroDeclaration string) int {
+	i, ok := inputColHeaders[macroDeclaration]
+	if !ok {
+		i = -1
+	}
+	return i
+}
+
+func arrayContains(arr []string, str string) bool {
+	i := 0
+	for i < len(arr) {
+		if str == arr[i] {
+			break
+		}
+		i += 1
+	}
+	return i < len(arr)
+
+}
+
+func addCommandMacro(cmd string, macro string) {
+	if !arrayContains(CommandMacros[cmd], macro) {
+		CommandMacros[cmd] = append(CommandMacros[cmd], macro)
+	}
+}
+
+func InitSessionLogMacros(sessionLog string) {
+	// this will create an entry for a command named "\nSessionLog",
+	// the newline will prevent any command named "SessionLog" in an ini file from overwriting it
+	InitMacros("\nSessionLog", sessionLog)
+}
+
+func InitMacros(cmd string, field string) {
+	_, exists := CommandMacros[cmd]
+	if !exists {
+		CommandMacros[cmd] = make([]string, 0)
+	}
+
+	rx, _ := regexp.Compile("\\{%.*?\\}")
+	rxenv, _ := regexp.Compile("\\{\\$.*?\\}")
+
+	for _, macro := range rx.FindAllString(field, -1) {
+		addCommandMacro(cmd, macro)
+	}
+	for _, macro := range rxenv.FindAllString(field, -1) {
+		addCommandMacro(cmd, macro)
+	}
+}
+
+func parseTimeModifier(arg string) (time.Duration, error) {
+	if len(arg) == 0 {
+		return time.Duration(0), nil
+	}
+
+	rx := regexp.MustCompile("([\\+\\-]\\d+)(.*)")
+	parsed := rx.FindStringSubmatch(arg)
+	if parsed == nil {
+		return time.Duration(0), errors.New(fmt.Sprintf("time modifier %s is not supported", arg))
+	} else {
+		mult, _ := strconv.Atoi(parsed[1]) // e.g -4, +4, 4
+		if parsed[2] == "MONTH" || parsed[2] == "MONTHS" {
+			return time.Duration(mult*30*24) * time.Hour, nil
+		} else if parsed[2] == "DAY" || parsed[2] == "DAYS" {
+			return time.Duration(mult) * 24 * time.Hour, nil
+		} else if parsed[2] == "HOUR" || parsed[2] == "HOURS" {
+			return time.Duration(mult) * time.Hour, nil
+		} else if parsed[2] == "MINUTE" || parsed[2] == "MINUTES" {
+			return time.Duration(mult) * time.Minute, nil
+		} else if parsed[2] == "SECOND" || parsed[2] == "SECONDS" {
+			return time.Duration(mult) * time.Second, nil
+		} else {
+			return time.Duration(0), errors.New(fmt.Sprintf("time modifier %s is not supported", arg))
+		}
+	}
+}
+
+func initUnixtimeMacro(macro string) {
+	declaration := macro[2 : len(macro)-1]
+	if strings.HasPrefix(declaration, "UNIXTIME") {
+		arg := declaration[8:]
+		rx1, _ := regexp.Compile("%(\\d+)?x")
+		fmtmatch := rx1.FindString(arg)
+		format := "%d"
+		if len(fmtmatch) > 0 {
+			format = fmtmatch
+			arg = strings.Replace(arg, fmtmatch, "", -1)
+		}
+		duration, err := parseTimeModifier(arg)
+		if err != nil {
+			log.Fatalf("UNIXTIME macro %s: %s", declaration, err.Error())
+		} else {
+			UnixtimeMacros[macro] = UnixtimeMacro{duration, format}
+		}
+	}
+}
+
+func initPrintTimeMacro(macro string) {
+	declaration := macro[2 : len(macro)-1]
+	if strings.HasPrefix(declaration, "TIME") {
+		format := "2006-01-02 15:04:05"
+		arg := declaration[4:]
+		duration, err := parseTimeModifier(arg)
+		if err != nil {
+			log.Fatalf("TIME macro %s: %s", declaration, err.Error())
+		} else {
+			PrintTimeMacros[macro] = UnixtimeMacro{duration, format}
+		}
+	}
+}
+
+func InitUnixtimeMacros() {
+	PrintTimeMacros = make(map[string]UnixtimeMacro)
+	UnixtimeMacros = make(map[string]UnixtimeMacro)
+	for _, macros := range CommandMacros {
+		for _, macro := range macros {
+			initUnixtimeMacro(macro)
+			initPrintTimeMacro(macro)
+		}
+	}
+	for _, macros := range Base64Macros {
+		for _, macro := range macros {
+			initUnixtimeMacro(macro)
+			initPrintTimeMacro(macro)
+		}
+	}
+	for _, macros := range Md5Macros {
+		for _, macro := range macros {
+			initUnixtimeMacro(macro)
+			initPrintTimeMacro(macro)
+		}
+	}
+}
+
+func addMd5Macro(cmd string, macro string) {
+	if !arrayContains(Md5Macros[cmd], macro) {
+		Md5Macros[cmd] = append(Md5Macros[cmd], macro)
+	}
+}
+
+func InitMd5Macro(cmd string, md5Input string) {
+	if len(md5Input) == 0 {
+		return
+	}
+
+	Md5Inputs[cmd] = md5Input
+	Md5Macros[cmd] = make([]string, 0)
+
+	rx, _ := regexp.Compile("\\{%.*?\\}")
+	rxenv, _ := regexp.Compile("\\{\\$.*?\\}")
+
+	for _, macro := range rx.FindAllString(md5Input, -1) {
+		addMd5Macro(cmd, macro)
+	}
+	for _, macro := range rxenv.FindAllString(md5Input, -1) {
+		addMd5Macro(cmd, macro)
+	}
+}
+
+func addBase64Macro(cmd string, macro string) {
+	if !arrayContains(Base64Macros[cmd], macro) {
+		Base64Macros[cmd] = append(Base64Macros[cmd], macro)
+	}
+}
+
+func InitBase64Macro(cmd string, base64Input string) {
+	if len(base64Input) == 0 {
+		return
+	}
+
+	Base64Inputs[cmd] = base64Input
+	Base64Macros[cmd] = make([]string, 0)
+
+	rx, _ := regexp.Compile("\\{%.*?\\}")
+	rxenv, _ := regexp.Compile("\\{\\$.*?\\}")
+
+	for _, macro := range rx.FindAllString(base64Input, -1) {
+		addBase64Macro(cmd, macro)
+	}
+	for _, macro := range rxenv.FindAllString(base64Input, -1) {
+		addBase64Macro(cmd, macro)
+	}
+}
+
+func _runnerMacro(command string, declaration string, inputData string, sessionVars map[string]string, reqTime time.Time) string {
+	if !(strings.HasPrefix(declaration, "{%") || strings.HasPrefix(declaration, "{$")) || !strings.HasSuffix(declaration, "}") {
+		return ""
+	}
+
+	uxt, ok := UnixtimeMacros[declaration]
+	prt, ok1 := PrintTimeMacros[declaration]
+	if ok {
+		timestamp := reqTime.Add(uxt.duration).UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond))
+		rx, _ := regexp.Compile("%(\\d+)x")
+		fmtdigits := rx.FindStringSubmatch(uxt.format)
+		if len(fmtdigits) == 0 {
+			return fmt.Sprintf(uxt.format, timestamp)
+		} else {
+			fmtnum, _ := strconv.Atoi(fmtdigits[1])
+			if fmtnum >= 12 {
+				return fmt.Sprintf(uxt.format, timestamp)
+			} else {
+				tmp := fmt.Sprintf("%012x", timestamp)
+				return tmp[0:fmtnum]
+			}
+		}
+	} else if ok1 {
+		return reqTime.Add(prt.duration).Format(prt.format)
+	} else if declaration == "{%KEY}" {
+		arr := strings.Split(inputData, KvDelimeter)
+		return arr[0]
+	} else if declaration == "{%VAL}" {
+		arr := strings.Split(inputData, KvDelimeter)
+		if len(arr) > 1 {
+			return arr[1]
+		} else {
+			return ""
+		}
+	} else if declaration == "{%MD5SUM}" {
+		testMd5 := Md5Inputs[command]
+		for _, macro := range Md5Macros[command] {
+			testMd5 = strings.Replace(testMd5, macro, runnerMacro(command, macro, inputData, sessionVars, reqTime), -1)
+		}
+		return strings.ToUpper(fmt.Sprintf("%x", md5.Sum([]byte(testMd5))))
+	} else if declaration == "{%BASE64ENC}" {
+		base64In := Base64Inputs[command]
+		for _, macro := range Base64Macros[command] {
+			base64In = strings.Replace(base64In, macro, runnerMacro(command, macro, inputData, sessionVars, reqTime), -1)
+		}
+		return base64.StdEncoding.EncodeToString([]byte(base64In))
+	} else if strings.HasPrefix(declaration, "{$") {
+		// an env var macro like {$SECRET}
+		return os.Getenv(declaration[2 : len(declaration)-1])
+	} else {
+		i := -1
+		// csvrx is package global, defined at top of the file, regexp.MustCompile("{%CSV\\[(\\d+)\\]}")
+		csvIndex := csvrx.FindStringSubmatch(declaration)
+		if len(csvIndex) > 0 {
+			i, _ = strconv.Atoi(csvIndex[1])
+		} else {
+			i = inputColumnIndex(declaration)
+		}
+		if i >= 0 {
+			arr := strings.Split(inputData, KvDelimeter)
+			return arr[i]
+		} else if declaration[1] == '%' {
+			session_var := declaration[2 : len(declaration)-1]
+			val, ok := sessionVars[session_var]
+			if ok {
+				return val
+			}
+		}
+	}
+	return ""
+}
+
+func runnerMacro(command string, declaration string, inputData string, sessionVars map[string]string, reqTime time.Time) string {
+	if !(strings.HasPrefix(declaration, "{%") || strings.HasPrefix(declaration, "{$")) || !strings.HasSuffix(declaration, "}") {
+		return ""
+	}
+
+	ssrx, _ := regexp.Compile("\\[(\\d+):(\\d+)\\]}")
+	declSubstr := ssrx.FindStringSubmatch(declaration)
+	if len(declSubstr) == 0 {
+		return _runnerMacro(command, declaration, inputData, sessionVars, reqTime)
+	} else {
+		declaration = strings.Replace(declaration, declSubstr[0], "}", 1)
+		result := _runnerMacro(command, declaration, inputData, sessionVars, reqTime)
+		ss0, _ := strconv.Atoi(declSubstr[1])
+		ss1, _ := strconv.Atoi(declSubstr[2])
+		return result[ss0:ss1]
+	}
+}
+
+func RunnerMacros(command string, inputData string, sessionVars map[string]string, reqTime time.Time, field string) string {
+	for _, macro := range CommandMacros[command] {
+		field = strings.Replace(field, macro, runnerMacro(command, macro, inputData, sessionVars, reqTime), -1)
+	}
+	return field
+}
+
+func RunnerMacrosRegexp(command string, inputData string, sessionVars map[string]string, reqTime time.Time, field string) string {
+	for _, macro := range CommandMacros[command] {
+		replacement := regexp.QuoteMeta(runnerMacro(command, macro, inputData, sessionVars, reqTime))
+		field = strings.Replace(field, macro, replacement, -1)
+	}
+	return field
+}
+
+func SessionLogMacros(inputData string, sessionVars map[string]string, logTime time.Time, initial string) string {
+	return RunnerMacros("\nSessionLog", inputData, sessionVars, logTime, initial)
+}
