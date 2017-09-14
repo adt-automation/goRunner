5ec1a734437320c8b056d4a5248d16f85351edd6
douglaswatson@gmail.com <douglaswatson@gmail.com>
2017-09-14 15:52:44 -0500

    adding javascript macro support part1

---
+++ config.js
@@ -0,0 +1,9 @@
+function add(a, b, c) {
+    //all goRunner input arguments to javascript are strings
+    return parseFloat(a) + parseFloat(b) + parseFloat(c); //so cast to float before adding
+  }
+
+function sub(a, b) {
+    //all goRunner input arguments to javascript are strings
+    return parseFloat(a) - parseFloat(b); //so cast to float before subtracting
+  }
